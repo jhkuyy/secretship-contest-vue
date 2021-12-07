9189e1158676c53d06c05c3fa6d46c4356b8e6c4
Artem Soldatov <thearteman@gmail.com>
2021-12-08 02:11:12 +0300

    add Button

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="button"></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style lang="stylus" module>
+.button {
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,2 @@
-import AuthWidget from './AuthWidget.vue'
-
-export {
-  // eslint-disable-next-line import/prefer-default-export
-  AuthWidget,
-}
+export { default as AuthWidget } from './AuthWidget.vue'
+export { default as Button } from './Button.vue'
