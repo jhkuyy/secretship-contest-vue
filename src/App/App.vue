fcef529b8bc9b9275a1808976e8f31531af8d912
Artem Soldatov <thearteman@gmail.com>
2021-12-08 01:07:40 +0300

    fix merge bullshit

--- src/App/App.vue
+++ src/App/App.vue
@@ -1,48 +1,65 @@
 <template>
-  <img
-    src="../assets/logo.png"
-    alt="Vue logo"
-  >
   <p>{{ isAuthorized }}</p>
 
-  <AuthWidget telegram-bot-name="SecretshipContestVueBot" />
+  <AuthWidget
+    telegram-bot-name="SecretshipContestVueBot"
+    @auth="onAuth"
+  />
+
+  <RouterLink
+    v-for="link in links"
+    :key="link"
+    :to="{ name: link }"
+    :class="$style.link"
+  >
+    {{ link }}
+  </RouterLink>
+
+  <RouterView />
 </template>
 
 <script>
-import { defineComponent } from 'vue'
 import { storeToRefs } from 'pinia'
+import { defineComponent } from 'vue'
+import { RouterLink, RouterView, useRouter } from 'vue-router'
 import { AuthWidget } from '../components'
+import { Routes } from '../router'
 import { useUser } from '../store'
 
 export default defineComponent({
   components: {
     AuthWidget,
+    RouterView,
+    RouterLink,
   },
-
   setup() {
     const store = useUser()
+    const router = useRouter()
     const { isAuthorized } = storeToRefs(store)
-
+    const onAuth = async (user) => {
+      await store.login(user)
+      await router.push({ name: Routes.APP_LIST })
+    }
     return {
       isAuthorized,
+      onAuth,
     }
+  },
+  computed: {
+    links() {
+      return [
+        Routes.WELCOME,
+        Routes.APP_ADD,
+        Routes.APP_LIST,
+        Routes.PROFILE_EDIT,
+      ]
+    },
   },
 })
 </script>
 
-<style lang="stylus">
-#app {
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-
-  a, a:hover {
-    color: $colors.primary
-  }
-
-  *,
-  ::before,
-  ::after {
-    flex 0 0 auto
-  }
+<style module>
+.link {
+  margin-right: 15px
 }
 </style>
--- src/App/index.js
+++ src/App/index.js
@@ -1,1 +1,19 @@
-export { default } from './App.vue'
+import { createApp as _createApp } from 'vue'
+import { createRouter } from '../router'
+import { createStore } from '../store'
+import App from './App.vue'
+
+export default function createApp() {
+  const router = createRouter()
+  const store = createStore()
+
+  const app = _createApp(App)
+    .use(router)
+    .use(store)
+
+  return {
+    app,
+    router,
+    store,
+  }
+}
