7c5515bc6746ecc117f19d6e22ced7cbad6cfdaf
Artem Soldatov <thearteman@gmail.com>
2021-12-08 04:56:29 +0300

    update Button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,14 +1,63 @@
 <template>
-  <div :class="button"></div>
+  <router-link
+    v-if="to !== undefined"
+    :class="wrapperClasses"
+    custom
+  >
+    <template #default="{ href }">
+      <a :href="href">
+        <slot />
+      </a>
+    </template>
+  </router-link>
+
+  <button
+    v-else
+    :class="wrapperClasses"
+  >
+    <slot />
+  </button>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  props: {
+    to: { type: Object, default: undefined },
+    block: Boolean,
+  },
+
+  computed: {
+    wrapperClasses() {
+      return {
+        [this.$style.wrapper]: true,
+        [this.$style.block]: this.block,
+      }
+    },
+  },
+})
 </script>
 
 <style lang="stylus" module>
-.button {
+.wrapper {
+  border: none
+  font-size: 14px
+  line-height: 20px
+  font-weight: 600
+  padding: 9px 22px
+  background-color: #3c9ff0
+  transition: background-color .2s ease
+  text-transform: none
+  color: #fff
+  border-radius: 6px
+  user-select: none
+  white-space: nowrap
+  vertical-align: middle
+  text-align: center
+}
+
+.block {
+  width 100%
 }
 </style>
