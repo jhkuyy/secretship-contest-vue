ebe1bc62abdf5cf21bc7e0f2bceb906140e2fa8d
Artem Soldatov <thearteman@gmail.com>
2021-12-08 04:55:23 +0300

    use rich class names for production

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,41 @@
 import path from 'path'
+
 import { defineConfig } from 'vite'
-import eslintPlugin from 'vite-plugin-eslint'
-import vue from '@vitejs/plugin-vue'
-import babel from 'vite-babel-plugin'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import vitePluginEslint from 'vite-plugin-eslint'
+import vitePluginVue from '@vitejs/plugin-vue'
+import vitePluginBabel from 'vite-babel-plugin'
+import vitePluginVueI18n from '@intlify/vite-plugin-vue-i18n'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+// default:
+//  https://github.com/madyankin/postcss-modules/blob/master/src/generateScopedName.js
+function generateRichClassName(name, filename) {
+  // const hash = crypto.createHash('sha1').update(css).digest('hex').slice(0, 6)
+  const { groups: { fileName } } = /(?<fileName>\w+)(\.\w+)*(\.\w+)(\?.+)?$/.exec(filename)
+  return `${fileName}_${name}`
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    eslintPlugin(),
-    babel(),
-    vueI18n({
+    vitePluginVue(),
+    vitePluginEslint(),
+    vitePluginBabel(),
+    vitePluginVueI18n({
       include: path.resolve(__dirname, './src/i18n/locales/**'),
     }),
   ],
 
   server: {
-    port: 80,
+    port: 8080,
   },
 
   css: {
+    modules: {
+      generateScopedName: isDev ? generateRichClassName : undefined,
+    },
+
     preprocessorOptions: {
       stylus: {
         imports: [
