1e06221d64ff962164fd3f3976fe64a3060a77ac
Sergey <sy.borisenko@gmail.com>
2021-12-10 01:28:28 +0300

    - added tabs and blockquote

---
+++ src/components/Blockquote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="$style.root">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style lang="stylus" module>
+.root {
+  padding: 5px 17px;
+  margin: 0 0 17px;
+  font-size: 15px;
+  border-left: 5px solid #179cde;
+  line-height: 1.6;
+}
+</style>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,63 @@
+<template>
+  <div :class="$style.root">
+    <div
+      v-for="tab in items"
+      :key="tab.name"
+      :class="[$style.tab, active === tab && $style.active]"
+      @click="$emit('change', tab)"
+    >
+      {{ tab.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    active: {
+      type: Object,
+      default: null,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  emits: ['change'],
+})
+</script>
+
+<style lang="stylus" module>
+.root {
+  display: flex
+}
+
+.tab {
+  font-size: 14px
+  line-height: 16px
+  font-weight: 500
+  padding: 7px 15px
+  transition: all .2s ease
+  border-radius: 16px
+  color: darken($colors.primary, 10%)
+  -webkit-font-smoothing: auto
+  cursor: pointer
+
+  &:hover {
+    background-color: #f0f6fa;
+    text-decoration: none;
+  }
+
+  &.active {
+    background-color: $colors.primary
+    color: #fff
+  }
+
+  &:not(:first-child) {
+    margin-left: 4px
+  }
+}
+</style>
