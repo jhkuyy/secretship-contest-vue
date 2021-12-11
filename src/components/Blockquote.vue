37828ed14122859eebfc437db551de8b31bffa0a
Sergey <sy.borisenko@gmail.com>
2021-12-11 20:09:27 +0300

    - blockquote styles

--- src/components/Blockquote.vue
+++ src/components/Blockquote.vue
@@ -1,6 +1,12 @@
 <template>
   <div :class="$style.root">
-    <slot />
+    <div :class="$style.header">
+      <slot name="header" />
+    </div>
+
+    <p :class="$style.content">
+      <slot />
+    </p>
   </div>
 </template>
 
@@ -12,10 +18,35 @@ export default defineComponent({})
 
 <style lang="stylus" module>
 .root {
-  padding: 5px 17px
+  padding: 0px 15px
   margin: 0 0 17px
   font-size: 15px
-  border-left: 5px solid $colors.primary
   line-height: 1.6
+  position: relative
+  -webkit-font-smoothing: auto
+
+  &::before {
+    content: ''
+    position: absolute
+    left: 0
+    top: 0
+    bottom: 0
+    width: 2px
+    background-color: #0f9ae4
+    border-radius: 1px
+    box-shadow: 0 0 0 0.5px #0f9ae4
+  }
+}
+
+.header {
+  color: $colors.primary
+  font-weight: 500
+  padding-bottom: 4px
+  line-height: 18px
+}
+
+.content {
+  margin: 0
+  line-height: 18px
 }
 </style>
