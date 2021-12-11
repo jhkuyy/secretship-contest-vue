ef6002b147adad8fbc1d1837af4d61f8440ba185
Artem Soldatov <thearteman@gmail.com>
2021-12-11 19:48:31 +0300

    add Checkbox and Radio

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,39 @@
+<script>
+import {
+  h,
+  vModelCheckbox,
+  withDirectives,
+  defineComponent,
+  mergeProps,
+} from 'vue'
+
+export default defineComponent({
+  props: {
+    modelValue: { type: [Boolean, Array], default: false },
+    value: { type: String, default: undefined },
+  },
+
+  emits: ['update:modelValue'],
+
+  render() {
+    const { modelValue, ...props } = this.$props
+    return withDirectives(
+      h(
+        'input',
+        mergeProps({
+          class: this.$style.checkbox,
+          type: 'checkbox',
+          'onUpdate:modelValue': (value) => this.$emit('update:modelValue', value),
+        }, props),
+      ),
+      [[vModelCheckbox, modelValue]],
+    )
+  },
+})
+</script>
+
+<style lang="stylus" module>
+.checkbox {
+  display: block
+}
+</style>
