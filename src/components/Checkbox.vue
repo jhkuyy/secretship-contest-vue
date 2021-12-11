d7e250f97ec73337de62dadfb2d8b4414d37b113
Artem Soldatov <thearteman@gmail.com>
2021-12-11 20:57:05 +0300

    finish checkbox

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -16,24 +16,79 @@ export default defineComponent({
   emits: ['update:modelValue'],
 
   render() {
-    const { modelValue, ...props } = this.$props
-    return withDirectives(
+    const {
+      modelValue,
+      class: classes,
+      style,
+      ...checkboxProps
+    } = this.$props
+    const {
+      checkbox: checkboxClass,
+      wrapper: wrapperClass,
+      overlay: overlayClass,
+    } = this.$style
+
+    const overlay = h('div', { class: overlayClass })
+    const checkbox = withDirectives(
       h(
         'input',
         mergeProps({
-          class: this.$style.checkbox,
+          class: checkboxClass,
           type: 'checkbox',
           'onUpdate:modelValue': (value) => this.$emit('update:modelValue', value),
-        }, props),
+        }, checkboxProps),
       ),
       [[vModelCheckbox, modelValue]],
+    )
+
+    return h(
+      'div',
+      { class: [wrapperClass, classes], style },
+      [checkbox, overlay],
     )
   },
 })
 </script>
 
 <style lang="stylus" module>
+
+.wrapper {
+  display: inline-block
+  width: 18px
+  height: 18px
+  position: relative
+
+  &::before {
+    content: ''
+    position: absolute
+    width: 100%
+    height: 100%
+    border-radius: 3px
+    border: 2px solid $colors.gray500
+  }
+}
+
+.overlay {
+  position: absolute
+  width: 100%
+  height: 100%
+  border-radius: 3px
+  background-color: $colors.primary
+  background-image: url('../assets/images/check.svg')
+  background-size: cover
+  transition: all .12s ease
+  opacity: 0
+  pointer-events: none
+}
+
+.checkbox:checked + .overlay {
+  opacity: 1
+}
+
 .checkbox {
-  display: block
+  position: absolute
+  width: 100%
+  height: 100%
+  opacity: 0
 }
 </style>
