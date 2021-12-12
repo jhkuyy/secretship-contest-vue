a34b70d51ed9c1b64dc182fc3fc37dab96a864ac
Sergey <sy.borisenko@gmail.com>
2021-12-12 16:27:24 +0300

    - build

---
+++ docs/assets/AppDetailsSettings.80a359f1.js
@@ -0,0 +1,1 @@
+import{i as n,o as c,j as o,k as e,t as s,q as a,O as r}from"./vendor.0fc79ac1.js";const p={class:"my-3"},_={class:"my-3 text-secondary text-center"},d={setup(i){const{t}=n();return(l,m)=>(c(),o(r,null,[e("h4",p,s(a(t)("page.app_settings.title")),1),e("p",_,s(a(t)("page.app_settings.empty")),1)],64))}};export{d as default};
